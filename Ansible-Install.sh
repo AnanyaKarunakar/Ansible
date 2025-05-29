fb9db590c10990c6b8e1393393ac071dbc655d49
Karna <karunakark1729@gmail.com>
2025-05-29 13:10:43 +0530

    Ansible Install through shell script

---
+++ Ansible-Install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Colours
+G="\e[32m"
+R="\e[31m"
+Y="\e[33m"
+N="\e[0m"
+
+echo -e "$Y Checking if Ansible is already Installed... $N"
+
+#Check if Ansible is installed 
+if command -v ansible &>/dev/null
+    then
+        VERSION=$(ansible --version | head -n1)
+        echo -e "$GAnsible is already installed: $VERSION $N"
+        exit 0
+fi
+
+echo -e "$Y Ansible not found. Installing Ansible...$N"
+# Enable EPEL and install Ansible
+sudo yum install -y epel-release &>dev/null
+if [ $? -ne 0 ]; then
+    echo -e "$R Failed to enable EPEL repository. Exiting $N"
+    exit 1
+fi
+
+sudo yum install -y ansible &>/dev/null
+if [ $? -eq 0 ]; then
+    VERSION=$(ansible --version | head -n1)
+    echo -e "$G Ansible is installed succefully: $VERSION $N"
+else
+    eho -e "$R Ansible installation failed.$N"
+    exit 1
+fi
